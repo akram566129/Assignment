a56848d9715790dbcbfa84e1c503da5b7ddd2b4c
akram566129 <gmakramhossan566129@gmail.com>
2023-03-23 00:44:34 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,13 @@
+
+
+function multiplicationTable(num){
+    for(let i = 1; i <= 10; i++){
+        const product = num * i;
+        console.log(num + " x " + i + " = " + num * i);
+    }
+}
+
+
+multiplicationTable(3);
+
+multiplicationTable(7);
